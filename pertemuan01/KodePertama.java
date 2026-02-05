7a0504c2b6936ec18b31528a095ee60b21e65950
Nabila Indaswari <nabilaindaswari@gmail.com>
2026-02-05 15:35:00 +0700

    added pertemuan01

---
+++ pertemuan01/KodePertama.java
@@ -0,0 +1,18 @@
+/*
+Nama : Nabila Indaswari
+NIM : D1041241021
+
+*/
+
+
+
+
+package pertemuan01;
+
+public class KodePertama {
+    public static void main(String[] args)
+    {
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah program Java pertama saya.");
+    }
+}
---
+++ pertemuan01/KodePertama_latihan.java
@@ -0,0 +1,20 @@
+/*
+Nama : Nabila Indaswari
+NIM : D1041241021
+
+*/
+
+
+package pertemuan01;
+
+public class KodePertama_latihan {
+    public static void main(String[] args)
+    {
+        System.out.println("Nama        : Nabila Indaswari");
+        System.out.println("NIM         : D1041241021");
+        System.out.println("Semester    : 4");
+        System.out.println("Prodi       : Informatika");
+        
+        
+    }
+}
