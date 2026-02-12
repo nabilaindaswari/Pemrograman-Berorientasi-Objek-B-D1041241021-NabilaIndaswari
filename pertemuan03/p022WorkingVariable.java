47b2e0bf3b21bbb37bb8def40b6d99ba81a184c0
dasaa <nabilaindaswari@gmail.com>
2026-02-12 14:05:17 +0700

    adding pertemuan03

---
+++ pertemuan03/p022WorkingVariable.java
@@ -0,0 +1,24 @@
+/*
+Nama : Nabila Indaswari
+NIM : D1041241021
+
+*/
+
+
+package pertemuan03;
+public class p022WorkingVariable {
+    public static void main(String[] args)
+    {
+        //Deklarasi dan inisialisasi Variable
+        int angka = 10;
+
+        //Menampilkan nilai awal variable
+        System.out.println("Nilai awal angka: " + angka);
+
+        //Mengubah nilai variable
+        angka = 25;
+
+        //Menampilkan nilai setelah perubahan
+        System.out.println("Nilai awal angka: " + angka);
+    }
+}
---
+++ pertemuan03/p023PrimitiveData.java
@@ -0,0 +1,34 @@
+/*
+Nama : Nabila Indaswari
+NIM : D1041241021
+
+*/
+
+
+
+public class p023PrimitiveData {
+    public static void main(String[] args)
+    {
+        // Tipe data bilangan bulat
+        int umur = 20;
+        long populasi = 8000000000L;
+        
+        //float tinggi = 170.5f;
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+
+        //tipe data karakter
+        char nilaiHuruf = 'A';
+
+        //tipe data logika
+        boolean lulus = true;
+
+        //menampilkan nilai semua variable
+        System.out.println("Umur            : " + umur);
+        System.out.println("Populasi        : " + populasi);
+        System.out.println("Tinggi Badan    : " + tinggi);
+        System.out.println("IPK             : " + ipk);
+        System.out.println("Nilai Huruf     : " + nilaiHuruf);
+        System.out.println("Status Lulus    : " + lulus);
+    }
+}
